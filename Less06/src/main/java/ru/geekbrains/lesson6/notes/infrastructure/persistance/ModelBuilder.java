e4321f50bcd69d7b895c55ed7b700e8162846730
Rinat <rinsham55@gmail.com>
2024-09-08 22:31:08 +0300

    Add Less06 (diagram)

---
+++ Less06/src/main/java/ru/geekbrains/lesson6/notes/infrastructure/persistance/ModelBuilder.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.lesson6.notes.infrastructure.persistance;
+
+public class ModelBuilder {
+    public ModelBuilder applyConfiguration(
+            ModelConfiguration configuration) {
+        //TODO: добавление конфигурации маппинга объекта
+        // некоторого типа к структуре таблицы БД
+        return this;
+    }
+}
